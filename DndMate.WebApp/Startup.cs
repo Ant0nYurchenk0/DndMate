411b2484934c8553391d52426d119917e6020fb8
Ant0nYurchenk0 <an.yurchenko.an@gmail.com>
2022-06-04 17:39:38 +0300

    initial commit

---
+++ DndMate.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DndMate.WebApp.Startup))]
+namespace DndMate.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
